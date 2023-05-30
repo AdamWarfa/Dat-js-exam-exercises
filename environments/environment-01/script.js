7f0af4e7df69ac3fcf5f63c8f1403722cbdec911
AdamWarfa <Adamwarfadk@gmail.com>
2023-05-30 11:05:27 +0200

    Update script.js

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,30 @@
 "use strict";
+window.addEventListener("load", initApp);
 
+async function initApp() {
+  const users = await getUsers("https://raw.githubusercontent.com/AdamWarfa/Dat-js-exam-exercises/main/environments/environment-01/users.json");
+  loopUsers();
+  function loopUsers() {
+    for (const user of users) {
+      if (user.active === true) addUsers(user);
+    }
+  }
+}
+
+async function getUsers(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+function addUsers(user) {
+  document.querySelector("#userlist").insertAdjacentHTML(
+    "beforeend",
+    /*html*/ `
+
+<li>Name: ${user.name}</li>
+<li>Active: ${user.active}</li>
+<br>
+`
+  );
+}
